312ee1d3840d9e8ae85a55496ff0279ddbcf9eea
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2023-11-05 20:22:03 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,17 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Liudmyla Kostiuk and it is
+        {" "}
+        <a
+          href="https://github.com/liudmyla2/weather-app-react"
+          target="_blank"
+        >open sourced on GitHub</a>
+      </footer>
+    </div>
+  );
 }
