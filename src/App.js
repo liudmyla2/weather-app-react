ff8341a9bfce5f8ccafae2e868d64d3ce7093a2b
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2023-11-07 20:15:09 +0200

    added API

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New Yourk" />
       <footer>
         This project was coded by Liudmyla Kostiuk and it is
         {" "}
