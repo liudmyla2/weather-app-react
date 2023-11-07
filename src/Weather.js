981e57459db418a2f6aabe407021541353f6276f
liudmyla2 <131667474+liudmyla2@users.noreply.github.com>
2023-11-07 19:50:32 +0200

    added axious

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,17 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
+
 export default function Weather() {
+    function handleResponse(response) {
+        console.log(response.data)
+    }
+
+
+    const apiKey = "be81f193e065bf5feb2d944c7336968b";
+    let city = "New York";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
     return (
       <div className="Weather">
         <form>
@@ -22,30 +33,32 @@ export default function Weather() {
             </div>
           </div>
         </form>
-        <h1>New York</h1>
+        <h1>{weatherData.city}</h1>
         <ul>
-          <li>Wednesday 07:00</li>
-          <li>Mostly cloudy</li>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix">
               <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="Mostly cloudy"
+                src={weatherData.iconUrl}
+                alt={weatherData.description}
                 className="float-left"
               ></img>
               <div className="float-left">
-                <span className="temperature">6</span>
+                <span className="temperature">
+                  {Math.round(weatherData.temperature)}
+                </span>
                 <span className="unit">°C</span>
               </div>
             </div>
           </div>
           <div className="col-6">
             <ul>
-              <li>Precipitation: 15%</li>
-              <li>Humidity: 72%</li>
-              <li>Wind: 13 km/h</li>
+              <li>Precipitation: {weatherData.precipitation}%</li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
             </ul>
           </div>
         </div>
